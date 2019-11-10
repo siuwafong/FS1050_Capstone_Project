e720da6409600c706b81ac1a6c6b27dbf6dca4fe
wilsonfong82 <wilsonfong82@gmail.com>
2019-11-10 15:53:45 -0500

    add home and register pages

--- app.js
+++ app.js
@@ -32,3 +32,27 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.render("landing");
 });
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/results", (req, res) => {
+  res.render("results");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
